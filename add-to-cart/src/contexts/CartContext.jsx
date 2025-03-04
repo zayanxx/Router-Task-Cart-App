f9c834e34f5d31a7d7d24598b71964d981791077
Zayan <smiliesanjay@gmail.com>
2025-03-04 17:34:08 +0530

    Update CartContext.jsx

--- add-to-cart/src/contexts/CartContext.jsx
+++ add-to-cart/src/contexts/CartContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-// eslint-disable-next-line react-refresh/only-export-components
+
 export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
